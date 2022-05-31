80c60f508fffb195a21e30af94b6044d187823b5
Roland-Sankara <roland1sankara@gmail.com>
2022-05-31 14:30:12 +0300

    update css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ function Header(){
                 }
             </div>
 
-            <nav style={{display: mobile?'none':'block'}}>
+            <nav style={{display: mobile?'block':'none'}}>
                 <ul className="navlist">
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
