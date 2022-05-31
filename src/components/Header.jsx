197c3986b2fedb21ba7b5831b684fa8217bd95fa
Roland-Sankara <roland1sankara@gmail.com>
2022-05-31 14:24:19 +0300

    fix the mobile navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Header(){
+    const [mobile, setMobile] = useState(false);
+
     return (
         <header>
             <a href="#" className="logo">
                 <h1>A|A</h1>
             </a>
 
-            <div id="menu-icon"> 
-                <i className="fa-solid fa-bars"></i> 
-                {/* <i className="fa-solid fa-x"></i> */}
+            <div id="menu-icon" onClick={()=>{setMobile((mobile)=>mobile?false:true)}}> 
+                {
+                    mobile ? <i className="fa-solid fa-bars"></i> : <i className="fa-solid fa-x"></i>
+                }
             </div>
 
-            <nav>
+            <nav style={{display: mobile?'none':'block'}}>
                 <ul className="navlist">
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
